4e06a2ae6f6cb32a3cc64976321389c79845378e
Jiaqing Xie <55912913+jiaqingxie@users.noreply.github.com>
2022-12-16 17:10:01 +0100

    Update MLP.py

--- MLP.py
+++ MLP.py
@@ -2,19 +2,18 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Four-Layer network as described in GANkin but adapted to the use of StyleGAN, with additional dropout
+# Four-Layer network as described in GANkin, with additional dropout
 class FourLayerNet(nn.Module):
     def __init__(self):
         super(FourLayerNet, self).__init__()
-        self.fc1 = nn.Linear(18*512*2, 18*512*2)
-        self.fc2 = nn.Linear(18*512*2, 18*512*2)
-        self.fc3 = nn.Linear(18*512*2, 18*512*2)
-        self.fc4 = nn.Linear(18*512*2, 18*512)
+        self.fc1 = nn.Linear(512, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 256)
+
         self.dropout = nn.Dropout(0.25)
 
     # x represents our data
     def forward(self, x):
-        x = nn.Flatten(x)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.dropout(x)
@@ -22,7 +21,4 @@ class FourLayerNet(nn.Module):
         x = F.relu(x)
         x = self.dropout(x)
         x = self.fc3(x)
-        x = F.relu(x)
-        x = self.dropout(x)
-        x = self.fc4(x)
         return x
