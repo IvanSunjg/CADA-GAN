45c8bea8120fa044cc8788b1e91b2c46f8940385
Sofie <92796816+sdaniels24@users.noreply.github.com>
2022-12-16 15:36:46 +0100

    Update MLP.py
    Update dims to StyleGAN
    Flatten

--- MLP.py
+++ MLP.py
@@ -2,18 +2,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Four-Layer network as described in GANkin, with additional dropout
+# Four-Layer network as described in GANkin but adapted to the use of StyleGAN, with additional dropout
 class FourLayerNet(nn.Module):
     def __init__(self):
         super(FourLayerNet, self).__init__()
-        self.fc1 = nn.Linear(256, 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 256)
-        self.fc4 = nn.Linear(256, 128)
+        self.fc1 = nn.Linear(18*512*2, 18*512*2)
+        self.fc2 = nn.Linear(18*512*2, 18*512*2)
+        self.fc3 = nn.Linear(18*512*2, 18*512*2)
+        self.fc4 = nn.Linear(18*512*2, 18*512)
         self.dropout = nn.Dropout(0.25)
 
     # x represents our data
     def forward(self, x):
+        x = nn.Flatten(x)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.dropout(x)
@@ -24,5 +25,4 @@ class FourLayerNet(nn.Module):
         x = F.relu(x)
         x = self.dropout(x)
         x = self.fc4(x)
-
         return x
