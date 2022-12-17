c9bdf418ac0eb6233869d7857bde70586997301b
Sofie <92796816+sdaniels24@users.noreply.github.com>
2022-12-17 09:16:35 +0100

    Update main.py
    Update image segmentation and desegmentation

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 import logging
 import numpy as np
 import torch
+import os
 from augmentations import augmentations as A
 from augmentations.TSKinFace_Dataset import TSKinDataset
 from torchvision.datasets import ImageFolder
@@ -15,6 +16,7 @@ from ImageSegmentation.pix2pixGAN.test import test_pix2pix
 from MLP import FourLayerNet
 
 import matplotlib.pyplot as plt
+import imageio
 
 from PipelineGAN.stylegan_train import load, embedding_function, style_transfer
 from PipelineGAN.utils import PSNR, loss_function
@@ -91,6 +93,7 @@ def main(args):
     
    # Use Image Segmentation.
     if args.segment:
+        # logging.info("Image Segmentation is being applied to the parent images.
         data_in = []
         label_list = []
         for images, labels in dataset:
@@ -98,18 +101,45 @@ def main(args):
             label_list.append(labels)
         data_in = np.asarray(data_in)
 
-        # Segment all images (child, mother, father)
+        # Segment all images (father, mother, child)
         data_out = face_parsing_test(input_images=data_in, blurring=args.segment-1)
         
-        # List of original images, segmented images, labels
-        data_list_seg = list(zip(data_in, data_out, label_list))      
+        seg_path = args.data_path + args.dataset + '_seg/'
+        father_path = seg_path + args.dataset + '-F/'
+        mother_path = seg_path + args.dataset + '-M/'
+        child_path = seg_path + args.dataset + '-Z/'
+        if not os.path.exists(father_path):
+            os.makedir(father_path)
+        if not os.path.exists(mother_path):
+            os.makedir(mother_path)
+        if not os.path.exists(child_path):
+            os.makedir(child_path)
+        for i, (im, label) in enumerate(list(zip(data_out, label_list))):
+            if label == 0:
+                imageio.imwrite(father_path + args.dataset + "-{}-F.png".format(i + 1), im)
+            elif label == 1:
+                imageio.imwrite(mother_path + args.dataset + "-{}-M.png".format(i + 1), im)
+            elif i%2 == 0 and label == 2 and args.dataset == 'FMSD':
+                imageio.imwrite(child_path + args.dataset + "-{}-D.png".format((i + 2)/2), im)
+            elif i%2 == 1 and label == 2 and args.dataset == 'FMSD':
+                imageio.imwrite(child_path + args.dataset + "-{}-S.png".format((i + 1/2)), im)
+            elif label == 2:
+                imageio.imwrite(child_path + args.dataset + "-{}-".format(i + 1) + args.dataset[-1] + ".png", im)
+        
+        dataset_orig = dataset.copy()
+        dataset = ImageFolder(
+            root = seg_path,
+            transform = transforms.Compose([
+                transforms.CenterCrop((1024, 1024)),
+                transforms.ToTensor(),
+            ])
+        )    
       
     # GAN projection into latent space
     # TODO: Jiaqing Xie
     if args.gan == "image2stylegan":
 
         _, g_synthesis = load(args)
-
 
         idx = 0
         c_idx = []
@@ -142,16 +172,12 @@ def main(args):
 
             break
 
-
     # Feature selection
     # TODO: Jiaqing Xie please check if it works
     model_f = FourLayerNet().to(args.device)
     model_m = FourLayerNet().to(args.device)
     loss_f = torch.nn.MSELoss(reduction='sum')
     optim = torch.optim.Adam(list(model_f.parameters()) + list(model_m.parameters()),  lr=args.lr)
-
-
-
 
     true_child_img = dataset[c_idx[0]][0]
     true_child_img = true_child_img[None, :]
@@ -160,10 +186,7 @@ def main(args):
     img_p = image.clone()
     img_p = upsample(img_p)
 
-
-    for i in range(args.epochs):
-
- 
+    for i in range(args.epochs): 
         optim.zero_grad()
         new_latent_f = model_f(latent_f)
         new_latent_m = model_m(latent_m)
@@ -187,26 +210,27 @@ def main(args):
     
     # Undo image segmentation
     # TODO: Sofie Daniëls
-    # parents → list of tuples: original image, segmented image
-    data_list_fathers = []
-    data_list_mothers = []
-    # children → list of quadruplets: original image, segmented image, generated segmented image
-    data_list_GAN_out = []
     if args.segment:
         data_list_seg_gen = []
         data_list_real = []
-        data_list_seg = []
-        for (orig, seg, gen) in data_list_GAN_out:
-            data_list_seg_gen.append(np.transpose(gen.numpy(), (1, 2, 0)))
-            data_list_real.append(orig)
-            data_list_seg.append(seg)
+        for im, label in syn_child_img:
+            data_list_seg_gen.append(np.transpose(im.numpy(), (1, 2, 0)))
+            data_list_real.append(np.transpose(next(iter(dataset_orig)).numpy(), (1, 2, 0)))
 
         data_list_real_gen = test_pix2pix([data_list_seg_gen, data_list_real], args.model, 'Output/')
 
-        # list of octoplets of following images:
-        # F orig, F seg, M orig, M seg, C orig, C seg, C generated segmentation, C generated real
-        # (With F = father, M = mother, C = child, seg = segmented, orig = original)
-        data_list_final = list(zip(data_list_fathers, data_list_mothers, data_list_real, data_list_seg, data_list_seg_gen, data_list_real_gen))
+        result_path = args.data_path + args.dataset + '_result/'
+        if not os.path.exists(result_path):
+            os.makedir(result_path)
+        for i, im in enumerate(data_list_real_gen):
+            if args.dataset == 'FMSD':
+                if i%2 == 0:
+                    imageio.imwrite(result_path + args.dataset + "-{}-D.png".format((i + 2/2)), im)
+                else:
+                    imageio.imwrite(result_path + args.dataset + "-{}-S.png".format((i + 1/2)), im)
+            else:
+                imageio.imwrite(child_path + args.dataset + "-{}-".format(i + 1) + args.dataset[-1] + ".png", im)
+
 
 
 
@@ -214,27 +238,37 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     # Include the arguments you want to experiment here
-    ## TODO: Jiaqing Xie
-    parser.add_argument("--augment", action=argparse.BooleanOptionalAction, help="argument to decide if you are going to use Image Augmentation", type=bool)
-    parser.add_argument("--mixup", action=argparse.BooleanOptionalAction, help="argument to decide if you are going to use Mixup", type=bool)
-    parser.add_argument("--augmix", action=argparse.BooleanOptionalAction, help="argument to decide if you are going to use AugMix", type=bool)
-    parser.add_argument("-p","--P", default=1.0, help="argument to decide the propability of using the Augmentation",type=float)
-    parser.add_argument("--smart", action=argparse.BooleanOptionalAction, help="argument to decide if you are going to use SmartAugment", type=bool)
-    parser.add_argument("--segment", default=0, help="Segmentation type: 0 is no segmentation, 1 is color segmentation, 2 is mix, 3 is blurred segmentation", type=int, choices=[0, 1, 2, 3])
-    parser.add_argument("--model", help="Path to pretrained pix2pix GAN model", type=str)
-    parser.add_argument("--gan", default="image2stylegan", type=str, help="gan type we used to generate images")
+    parser.add_argument("--augment", action=argparse.BooleanOptionalAction, type=bool,
+                        help="argument to decide if you are going to use Image Augmentation")
+    parser.add_argument("--mixup", action=argparse.BooleanOptionalAction, type=bool,
+                        help="argument to decide if you are going to use Mixup")
+    parser.add_argument("--augmix", action=argparse.BooleanOptionalAction, type=bool,
+                        help="argument to decide if you are going to use AugMix")
+    parser.add_argument("--P", "-p", default=1.0, type=float,
+                        help="argument to decide the propability of using the Augmentation")
+    parser.add_argument("--smart", action=argparse.BooleanOptionalAction, type=bool,
+                        help="argument to decide if you are going to use SmartAugment")
+    parser.add_argument("--segment", default=0, type=int, choices=[0, 1, 2, 3],
+                        help="Segmentation type: 0 is no segmentation, 1 is color segmentation, 2 is mix, 3 is blurred segmentation")
+    parser.add_argument("--model", type=str, help="Path to pretrained pix2pix GAN model")
+    parser.add_argument("--gan", default="image2stylegan", type=str,
+                        help="gan type we used to generate images")
     parser.add_argument("--batchsize", default=32,type=int, help="batch size")
-    parser.add_argument("--pretrain", action=argparse.BooleanOptionalAction, help="decide if you are going to use pretrained vgg model")
+    parser.add_argument("--pretrain", action=argparse.BooleanOptionalAction,
+                        help="decide if you are going to use pretrained vgg model")
     parser.add_argument("--use_cuda", action='store', default=True, type=bool)
     parser.add_argument("--model_dir", action='store', default="pretrain_stylegan", type=str)
-    parser.add_argument("--model_name", action='store', default="karras2019stylegan-ffhq-1024x1024.pt", type=str)
+    parser.add_argument("--model_name", action='store',
+                        default="karras2019stylegan-ffhq-1024x1024.pt", type=str)
     parser.add_argument("--lr", action='store', default=0.015, type=float)
     parser.add_argument("--epochs", action='store', default=1000, type=int)
     parser.add_argument("--device", default='cuda:0', help="whether use gpu or not")
-    parser.add_argument("--data_path", default='dataset/TSKinFace_Data/TSKinFace_cropped/', help="dataset path", type=str)
+    parser.add_argument("--data_path", default='dataset/TSKinFace_Data/TSKinFace_cropped/',
+                        type=str, help="dataset path")
 
     # Argument to decide which dataset to use
-    parser.add_argument("--dataset","-d",default="FMD", help="argument to decide which dataset to use. Default setting is FMD", type=str)
+    parser.add_argument("--dataset","-d",default="FMD", type=str,
+                        help="argument to decide which dataset to use. Default setting is FMD")
 
     # Parsing the argument to the args
     args = parser.parse_args()
@@ -242,20 +276,20 @@ if __name__ == "__main__":
     # Include some very basic sanity check here to make sure the codes does not crash inside main()
     ## TODO: Jiaqing Xie
     if args.P > 1.0 or args.P < 0.0:
-        raise ValueError("The Probability for argument -p has to be between 0.0 and 1.0!!")
-
-    if args.segment > 0 and args.model is None:
-        raise ValueError("Please specify the model path for the pix2pix GAN")
-
+        raise ValueError("The Probability for argument -p has to be between 0.0 and 1.0!")
+    
     if not args.augment:
         if args.mixup or args.augmix or args.smart:
-            raise ValueError("MixUp, AugMix or SmartAugment can only be used when you set the Augment flag!!!")
+            raise ValueError("MixUp, AugMix or SmartAugment can only be used when you set the Augment flag!")
     
     if args.augment:
         if not args.mixup and not args.augmix and not args.smart:
-            raise ValueError("One of MixUp, AugMix or SmartAugment has to be specified for your augmentation task!!!")
+            raise ValueError("One of MixUp, AugMix or SmartAugment has to be specified for your augmentation task!")
     
     if args.dataset not in ['FMD', 'FMSD', 'FMS']:
-        raise ValueError("Invalid argument setting for dataset. Only FMD, FMS or FMSD accepted!!")
+        raise ValueError("Invalid argument setting for dataset. Only FMD, FMS or FMSD accepted!")
+
+    if args.segment > 0 and args.model is None:
+        raise ValueError("Please specify the model path for the pix2pix GAN")
 
     main(args)
