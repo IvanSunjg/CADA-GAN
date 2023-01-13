36cd902c6bec872ff4eda56b6d03e6cdcaf18ff4
Sofie <92796816+sdaniels24@users.noreply.github.com>
2023-01-13 21:29:51 +0100

    Update MLP.py

--- MLP.py
+++ MLP.py
@@ -6,8 +6,8 @@ import torch.nn.functional as F
 class FourLayerNet(nn.Module):
     def __init__(self):
         super(FourLayerNet, self).__init__()
-        self.fc1 = nn.Linear(2*16*512, 16*512)
-        self.fc2 = nn.Linear(16*512, 16*512)
+        self.fc1 = nn.Linear(2*16*512, 512)
+        self.fc2 = nn.Linear(512, 16*512)
         self.unflatten = nn.Unflatten(1, torch.Size([16,512]))
         self.dropout = nn.Dropout(0.25)
 
